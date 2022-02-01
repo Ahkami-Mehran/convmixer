d55b3f20174eb433a024130bf9c0ab344c4093ac
Mehran Ahkami <ahkami.mehran@gmail.com>
2022-02-01 16:27:57 +0100

    add deit model as well as convmixer

--- pytorch-image-models/timm/models/__init__.py
+++ pytorch-image-models/timm/models/__init__.py
@@ -28,6 +28,7 @@ from .mobilenetv3 import *
 from .nasnet import *
 from .nest import *
 from .nfnet import *
+from .patchconvnet_models import *
 from .pit import *
 from .pnasnet import *
 from .regnet import *
